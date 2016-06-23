2cc3f5f5bd6190ad0bb9855c903ec6458c0c2171
bluefishforsale <terracnosaur@gmail.com>
2016-06-22 22:44:33 -0700

    first working version OMG!

--- python_clients/raver_plaid_pyOSC_control.py
+++ python_clients/raver_plaid_pyOSC_control.py
@@ -68,34 +68,40 @@ fps = args.fps         # frames per second
 
 def osc_color_handler(*args):
     queue = command_queue.__weakref__()
-    # path, tags, args, source
-    #print(args)
-    path = args[0]
-    data = args[2]
+    # args ==  path, [tags], args, source
     # tags will contain 'fff'
     # args is a OSCMessage with data
     # source is where the message came from (in case you need to reply)
+    #print(args)
+    path = args[0]
+    tags = args[1]
+    data = args[2]
+    #source = args[3]
     #queue.put((path, args[0], args[1]))
     #queue.put_nowait((path, data[0] - 0.5 * 1.5, data[1] - 0.5 * 1.5))
-    queue.put((path, data[0] - 0.5 * 1.5, data[1] - 0.5 * 1.5))
+    queue.put((path, data[0] - 0.5, data[1] - 0.5))
     return
 
 def render_pixels(n_pixels, client, start_time):
-    queue = command_queue.__weakref__()
-    #------------------------------------------------------------------------------
-    # initialize the values
-    freq_r = 0.01
-    freq_g = 0.01
-    freq_b = 0.01
-    speed_r = 0.01
-    speed_g = 0.01
-    speed_b = 0.01
-
     t = time.time() - start_time
+    queue = command_queue.__weakref__()
 
-    #name, speed, freq = queue.get_nowait()
-    name, speed, freq = queue.get()
-    pprint(name, speed, freq)
+    global speed_r
+    global speed_g
+    global speed_b
+    global freq_r
+    global freq_g
+    global freq_b
+    name = ""
+
+    if queue.empty():
+        queue.put(('/red', speed_r, freq_r))
+        queue.put(('/green', speed_g, freq_g))
+        queue.put(('/blue', speed_b, freq_b))
+    else:
+        name, speed, freq = queue.get_nowait()
+        print(name, speed, freq)
+
     if name is not None:
         if name == '/red':
             speed_r, freq_r = speed, freq
@@ -111,9 +117,9 @@ def render_pixels(n_pixels, client, start_time):
             speed_b = speed_b
             freq_b = freq_b
 
-    print(("%.2f" % t, speed_r, freq_r))
-    print(("%.2f" % t, speed_g, freq_g))
-    print(("%.2f" % t, speed_b, freq_b))
+    #print(("%.2f" % t, speed_r, freq_r))
+    #print(("%.2f" % t, speed_g, freq_g))
+    #print(("%.2f" % t, speed_b, freq_b))
 
     pixels = []
     for ii in range(n_pixels):
@@ -148,8 +154,7 @@ def render_pixels(n_pixels, client, start_time):
                 math.cos((t/speed_b + pct*freq_b)*math.pi*2),
                 -1, 1, 0, 256)
         pixels.append((r, g, b))
-    client.put_pixels(pixels, channel=0)
-    time.sleep( 1/fps )
+    return pixels
 
 
 if __name__ == '__main__':
@@ -165,6 +170,19 @@ if __name__ == '__main__':
     osc_server.addMsgHandler( "/black_offset",  osc_color_handler)
     osc_server.timeout=0
 
+    #------------------------------------------------------------------------------
+    # initialize the values
+    freq_r = 0.01
+    freq_g = 0.01
+    freq_b = 0.01
+    speed_r = 0.01
+    speed_g = 0.01
+    speed_b = 0.01
+
+    osc_color_handler("/red", 'ff', [freq_r, speed_r])
+    osc_color_handler("/green", 'ff', [freq_r, speed_r])
+    osc_color_handler("/blue", 'ff', [freq_r, speed_r])
+
     print("Listening for OSC on {}".format("%s:%d" % osc_server.server_address))
     print("Connecting to OPC on {}".format(OPC_IP_PORT))
     print('control-c to exit...')
@@ -173,6 +191,8 @@ if __name__ == '__main__':
     server_job.start()
 
     start_time = time.time()
-    while 1:
+    while True:
         #osc_server.handle_request()
-        render_pixels(args.pixel_count, client, start_time)
+        pixels = render_pixels(args.pixel_count, client, start_time)
+        client.put_pixels(pixels, channel=0)
+        time.sleep( 1/fps )
